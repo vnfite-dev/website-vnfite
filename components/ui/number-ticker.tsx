d967a291acd5d18d8d502ddbe5b772eae020c02f
AnhGoDev <anhnguyendinh263@gmail.com>
2025-01-09 11:43:28 +0700

    build

--- components/ui/number-ticker.tsx
+++ components/ui/number-ticker.tsx
@@ -6,50 +6,54 @@ import { useInView, useMotionValue, useSpring } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export default function NumberTicker({
-	value,
-	direction = "up",
-	delay = 0,
-	className,
-	decimalPlaces = 0,
+  value,
+  direction = "up",
+  delay = 0,
+  className,
+  decimalPlaces = 0,
 }: {
-	value: number;
-	direction?: "up" | "down";
-	className?: string;
-	delay?: number; // delay in s
-	decimalPlaces?: number;
+  value: number;
+  direction?: "up" | "down";
+  className?: string;
+  delay?: number; // delay in s
+  decimalPlaces?: number;
 }) {
-	const ref = useRef<HTMLSpanElement>(null);
-	const motionValue = useMotionValue(direction === "down" ? value : 0);
-	const springValue = useSpring(motionValue, {
-		damping: 60,
-		stiffness: 100,
-	});
-	const isInView = useInView(ref, { once: true, margin: "0px" });
+  const ref = useRef<HTMLSpanElement>(null);
+  const motionValue = useMotionValue(direction === "down" ? value : 0);
+  const springValue = useSpring(motionValue, {
+    damping: 60,
+    stiffness: 100,
+  });
+  const isInView = useInView(ref, { once: true, margin: "0px" });
 
-	useEffect(() => {
-		isInView &&
-			setTimeout(() => {
-				motionValue.set(direction === "down" ? 0 : value);
-			}, delay * 1000);
-	}, [motionValue, isInView, delay, value, direction]);
+  useEffect(() => {
+    if (isInView) {
+      setTimeout(() => {
+        motionValue.set(direction === "down" ? 0 : value);
+      }, delay * 1000);
+    }
+  }, [motionValue, isInView, delay, value, direction]);
 
-	useEffect(
-		() =>
-			springValue.on("change", (latest) => {
-				if (ref.current) {
-					ref.current.textContent = Intl.NumberFormat("en-US", {
-						minimumFractionDigits: decimalPlaces,
-						maximumFractionDigits: decimalPlaces,
-					}).format(Number(latest.toFixed(decimalPlaces)));
-				}
-			}),
-		[springValue, decimalPlaces]
-	);
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current) {
+          ref.current.textContent = Intl.NumberFormat("en-US", {
+            minimumFractionDigits: decimalPlaces,
+            maximumFractionDigits: decimalPlaces,
+          }).format(Number(latest.toFixed(decimalPlaces)));
+        }
+      }),
+    [springValue, decimalPlaces]
+  );
 
-	return (
-		<span
-			className={cn("inline-block tabular-nums text-black dark:text-white tracking-wider", className)}
-			ref={ref}
-		/>
-	);
+  return (
+    <span
+      className={cn(
+        "inline-block tabular-nums text-black dark:text-white tracking-wider",
+        className
+      )}
+      ref={ref}
+    />
+  );
 }
