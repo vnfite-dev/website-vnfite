b026d5deb1ca4235eeaee8189b950b0e61c67b92
NguyenVanAnit <annit1910@gmail.com>
2025-02-07 17:01:18 +0700

    add api oke

--- app/news/[...id]/page.tsx
+++ app/news/[...id]/page.tsx
@@ -1,66 +1,79 @@
-'use client';
-
 import Image from "next/image";
 import { Clock, Share2 } from "lucide-react";
 import React from "react";
-import { detailNews } from "../data";
-import { useParams, useRouter } from "next/navigation";
+// import { detailNews } from "../data";
+// import { useParams, useRouter } from "next/navigation";
+import { simpleFetchFunction } from "@/lib/utils";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+
+const fetchNewsData = async () => {
+	const data = await simpleFetchFunction(
+		`/get-news?pageSize=10&pageNumber=0&type=1`
+	);
+	return data.data;
+};
+
+const formatDate = (isoString: string | number | Date) => {
+	return new Date(isoString).toLocaleDateString("vi-VN");
+};
 
 const SuggestedNew = ({
-	image = "/images/news/suggestedNew.jpg",
-	title = "P2P Lending hút nhà đầu tư thời công nghệ 4.0",
-	time = "17:30 - 26/12/2024",
-	id = 1,
+	urlImage = "/images/news/suggestedNew.jpg",
+	mainTitle,
+	createdDate,
+	id,
 }: {
-	image?: string;
-	title?: string;
-	time?: string;
-	id?: number;
+	urlImage?: string;
+	mainTitle?: string;
+	createdDate?: string;
+	id?: string;
 }) => {
-	const router = useRouter();
-
-	const navigateToDetail = (id: number) => {
-		router.push(`/news/${id}`);
-	};
 
 	return (
-		<div className="flex gap-4 border-b border-b-[#E6E6E6] pb-4 cursor-pointer" onClick={() => navigateToDetail(id)}>
+		<Link className="flex gap-4 border-b border-b-[#E6E6E6] pb-4 cursor-pointer"
+			href={`/news/${id}`}
+		>
 			<div className="w-20 h-20 min-w-20 min-h-20 overflow-hidden relative rounded-lg">
-				<Image src={image} alt="Ảnh tin tức" objectFit="cover" layout="fill" className="" />
+				<Image
+					src={urlImage || '/images/news/suggestedNew.jpg'}
+					alt="Ảnh tin tức"
+					objectFit="cover"
+					layout="fill"
+					className=""
+				/>
 			</div>
 			<div className="">
-				<div className="font-medium text-base">{title}</div>
+				<div className="font-medium text-base">{mainTitle}</div>
 				<div className="flex justify-start items-center gap-2 text-[12px] mt-1">
 					<Clock size={14} />
-					{time}
+					{formatDate(createdDate || '')}
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
-const NewsDetail = () => {
-	const { id } = useParams();
+const NewsDetail = async ({ params }: { params: { id: string[] } }) => {
+	const newsData = await fetchNewsData();
+	const newsList = newsData?.data ?? [];
 
-	if (!id || id.length === 0) {
-		// Xử lý khi id không tồn tại hoặc không hợp lệ
-		return <div>Error: Invalid ID</div>;
+	if (!params || !params.id || !Array.isArray(params.id) || !params.id[0]) {
+		return notFound();
 	}
+	const id = params.id[0];
 
-	const news = detailNews.find((_) => _.id === Number(id[0]));
 
-	const formattedDetail = news?.detail.split('\n').map((line, index) => (
-		<span key={index}>{line}<br /></span>
-	));
+	const news = newsList.find((_: { id: string }) => _.id === id);
 
 	return (
 		<div className="px-[8%] lg:px-[6%] xl:px-[10%] 2xl:px-[16.7%] my-28 flex flex-col lg:flex-row">
 			<div className="font-semibold text-2xl lg:pr-10 xl:pr-14 w-full lg:w-[70%] 2xl:w-[65%]">
-				{news?.title}
+				{news?.mainTitle}
 				<div className="flex gap-12 mt-4 items-center text-sm">
 					<div className="flex items-center gap-3">
 						<Clock />
-						<p className="text-gray-600">{news?.time}</p>
+						<p className="text-gray-600">{formatDate(news?.createdDate)}</p>
 					</div>
 
 					<div className="flex gap-3 items-center">
@@ -70,39 +83,17 @@ const NewsDetail = () => {
 				</div>
 				<div className="w-full mt-6 mx-auto">
 					<Image
-						src={news?.image ?? ''}
+						src={news?.urlImage || "/images/news/suggestedNew.jpg"}
 						alt="rich"
 						width={764}
 						height={573}
 					/>
 				</div>
-				<div className="mt-4 text-base font-medium text-gray-700">
-					{formattedDetail || `Ngày 6/11 vừa qua, tại Cung Trí Thức, Thành phố Hà Nội, Câu Lạc Bộ Doanh Nhân Việt Nam - Asean
-					Global đã tổ chức lễ bổ nhiệm các lãnh đạo mới và công bố kế hoạch xúc tiến thương mại tại Úc
-					vào năm 2025. Buổi lễ đã thu hút sự tham gia của đông đảo doanh nhân, các lãnh đạo CLB, cùng
-					nhiều khách mời, trong đó có ông Nguyễn Ngọc Quang, Chủ tịch Hiệp hội Doanh nghiệp Nhỏ và Vừa
-					Ngành Nghề Nông Thôn Việt Nam (Varisme), và ông Đặng Đình Đức, Trưởng Ban Kinh Tế của Varisme.
-					Trong buổi lễ, Th.S Nguyễn Ngân, Chủ tịch CLB Doanh Nhân Việt Nam - Asean Global, đã chính thức
-					tuyên bố các quyết định bổ nhiệm lãnh đạo mới của CLB, nhằm tạo ra một đội ngũ điều hành mạnh
-					mẽ và đoàn kết hơn, tiếp tục dẫn dắt CLB phát triển vững mạnh. Cùng với đó, bà Nguyễn Ngân đã
-					trình bày chi tiết về kế hoạch xúc tiến thương mại tại Úc vào năm 2025, đánh dấu một bước ngoặt
-					quan trọng trong chiến lược quốc tế hóa và kết nối doanh nghiệp Việt Nam với thị trường toàn
-					cầu. Phát biểu tại sự kiện, ông Nguyễn Ngọc Quang, Chủ tịch Varisme, đã ghi nhận và đánh giá
-					cao những đóng góp thiết thực của Câu Lạc Bộ Doanh Nhân Việt Nam - Asean Global vào nền kinh tế
-					Việt Nam, đồng thời bày tỏ sự hỗ trợ mạnh mẽ của Hiệp hội Doanh nghiệp Nhỏ và Vừa Ngành Nghề
-					Nông Thôn trong việc thúc đẩy các chương trình xúc tiến thương mại quốc tế. Ông nhấn mạnh rằng,
-					dù Việt Nam hiện có rất nhiều sản phẩm chất lượng và phù hợp với nhu cầu của thị trường quốc
-					tế, nhưng chưa được quảng bá và xuất khẩu rộng rãi. Vì vậy, việc Câu Lạc Bộ triển khai chương
-					trình xúc tiến thương mại tại Úc và các thị trường quốc tế khác sẽ góp phần quan trọng vào việc
-					nâng cao giá trị thương hiệu Việt Nam. Chương trình cũng chứng kiến sự chia sẻ của nhiều doanh
-					nhân, đại diện cho các doanh nghiệp hoạt động trong các lĩnh vực khác nhau, cùng với mong muốn
-					chung tay thúc đẩy sự phát triển bền vững, đồng thời thể hiện sự tin tưởng vào định hướng và sự
-					lãnh đạo của CLB trong các hoạt động xúc tiến thương mại quốc tế sắp tới. VNFITE xin gửi lời
-					chúc mừng chân thành đến Câu Lạc Bộ Doanh Nhân Việt Nam - Asean Global và Ban Lãnh Đạo của CLB.
-					Chúng tôi tin rằng với sự nhiệt huyết và cam kết của các thành viên, CLB sẽ tiếp tục gặt hái
-					nhiều thành công trong các hoạt động xúc tiến thương mại, đưa sản phẩm Việt Nam ra thế giới và
-					tạo ra những cơ hội mới cho cộng đồng doanh nhân Việt Nam. Một số hình ảnh tại buổi lễ:`}
-				</div>
+				<div
+					className="mt-4 text-base font-medium text-gray-700"
+					dangerouslySetInnerHTML={{ __html: news?.content || `Không tìm thấy nội dung cho tin tức này` }}
+				/>
+
 				<div className="w-full h-auto relative mt-6">
 					<Image
 						// width={764}
@@ -118,10 +109,14 @@ const NewsDetail = () => {
 			{/* LEFT BAR */}
 			<div className="w-full lg:w-[30%] 2xl:w-[35%]">
 				<div className="sticky top-40 lg:top-0 w-full h-auto">
-					<div className="text-2xl font-semibold text-center ">Tin tức liên quan</div>
+					<div className="text-2xl font-semibold text-center ">
+						Tin tức liên quan
+					</div>
 
 					<div className="mt-4 lg:mt-12 space-y-4 lg:border-l-2 lg:border-gray-300 lg:pl-12 xl:pl-16">
-						{detailNews.slice(1, 5).map((news, index) => (<SuggestedNew key={index} {...news} />))}
+						{newsList.slice(1, 5).map((news: React.JSX.IntrinsicAttributes & { urlImage?: string; mainTitle?: string; createdDate?: string; id?: string; }, index: React.Key | null | undefined) => (
+							<SuggestedNew key={index} {...news} />
+						))}
 					</div>
 				</div>
 			</div>
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {
 	Carousel,
 	CarouselContent,
@@ -8,12 +6,13 @@ import {
 	CarouselPrevious,
 	CarouselDots
 } from "@/components/ui/carousel";
-import { cn } from "@/lib/utils";
-// import { time } from "console";
+import { cn, simpleFetchFunction } from "@/lib/utils";
 import { Calendar } from "lucide-react";
 import Image from "next/image";
-import { detailNews } from './data';
-import { useRouter } from "next/navigation";
+import { detailNews } from "./data";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { Key } from "react";
+import Link from "next/link";
 
 const bigNews = [
 	{
@@ -22,16 +21,23 @@ const bigNews = [
 		title: "THỂ LỆ CHƯƠNG TRÌNH “TẢI APP NGAY, CÓ TIỀN LIỀN TAY - NHẬN TIỀN MẶT ĐẾN 88 TRIỆU ĐỒNG”",
 	},
 ];
-const NewsPage = () => {
-	const router = useRouter();
 
-	const navigateToDetail = (id: number) => {
-		router.push(`/news/${id}`);
-	}
+const fetchNewsData = async () => {
+	const data = await simpleFetchFunction(`/get-news?pageSize=10&pageNumber=0&type=1`);
+	return data.data;
+};
+
+const NewsPage = async () => {
+	const newsData = await fetchNewsData();
+	const newsList = newsData?.data ?? [];
 
 	const truncateText = (text: string, length: number) => {
 		return text.length > length ? text.slice(0, length) + "..." : text;
-	}
+	};
+
+	const formatDate = (isoString: string | number | Date) => {
+		return new Date(isoString).toLocaleDateString("vi-VN");
+	};
 
 	return (
 		<div className="px-5 sm:px-[10%] xl:px-[16.7%] mx-auto">
@@ -43,7 +49,7 @@ const NewsPage = () => {
 				<Carousel className="w-full">
 					<CarouselContent>
 						{Array.from({ length: 5 }).map((_, index) => (
-							<CarouselItem key={index} className="">
+							<CarouselItem key={index}>
 								<div
 									className={cn(
 										"p-1 border border-red-300 rounded-2xl h-64 md:h-80 lg:h-96 xl:h-[458px] bg-cover flex justify-center items-end pb-6 md:pb-12 lg:pb-16",
@@ -71,7 +77,7 @@ const NewsPage = () => {
 							"hidden sm:flex w-full md:w-[80%] lg:w-[55%] xl:w-full cursor-pointer aspect-square bg-cover rounded-4xl relative overflow-hidden group mx-auto",
 							detailNews[0].banner
 						)}
-						onClick={() => navigateToDetail(1)}
+					// onClick={() => navigateToDetail(1)}
 					>
 						{/* Gradient Filter for Bottom Half */}
 						<div className="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-gray-800/100 to-transparent group-hover:h-full group-hover:from-red-600/65 transition-all duration-300 pointer-events-none"></div>
@@ -80,7 +86,7 @@ const NewsPage = () => {
 						<div className="relative z-10 flex justify-end items-center h-full px-7 pb-6 sm:pb-14 flex-col gap-4 overflow-hidden">
 							{/* Title Animation */}
 							<p className="text-white font-semibold text-lg sm:text-2xl transform translate-y-24 group-hover:-translate-y-0 transition-transform duration-500 ease-in-out">
-								{detailNews[0].title}
+								{newsList[0]?.mainTitle}
 							</p>
 
 							{/* Detail Animation */}
@@ -101,24 +107,26 @@ const NewsPage = () => {
 					</div>
 
 					<div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 sm:px-[18%] md:px-0 lg:px-[10%] xl:px-0">
-						{detailNews.slice(1, 5).map((_, index) => (
-							<div key={index} className="cursor-pointer group" onClick={() => navigateToDetail(_.id)}>
+						{newsList?.slice(1, 5).map((_: { urlImage: string | StaticImport; mainTitle: string; createdDate: string | number | Date; id: string }, index: Key | null | undefined) => (
+							<Link key={index} className="cursor-pointer group" href={`/news/${_?.id}`}
+							// onClick={() => navigateToDetail(_.id)}
+							>
 								<div
 									className={cn("w-full relative h-[160px] rounded-2xl bg-cover overflow-hidden")}
 								>
 									<Image
 										className="group-hover:scale-110 object-cover"
-										src={_.image}
+										src={_.urlImage}
 										alt="banner"
 										fill
 									/>
 								</div>
-								<div className="text-base font-semibold mt-2">{truncateText(_.title, 85)}</div>
+								<div className="text-base font-semibold mt-2">{truncateText(_.mainTitle, 85)}</div>
 								<div className="flex gap-1 text-sm items-center text-gray-600">
 									<Calendar size={16} />
-									{_.time}
+									{formatDate(_.createdDate)}
 								</div>
-							</div>
+							</Link>
 						))}
 					</div>
 				</div>
@@ -126,37 +134,23 @@ const NewsPage = () => {
 
 			<div className="mt-10 lg:mt-28">
 				<p className="text-center text-2xl lg:text-5xl font-semibold">Danh sách tin tức</p>
-
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 lg:gap-6 mt-6 lg:mt-16">
-					{detailNews.map((_, index) => (
-						<div
+					{newsList?.map((news: { urlImage: string | StaticImport; createdDate: string | number | Date; mainTitle: string; id: string }, index: Key | null | undefined) => (
+						<Link
+							href={`/news/${news?.id}`}
 							key={index}
 							className="flex flex-col gap-3 lg:gap-6 p-2 pb-4 lg:pb-6 border-2 rounded-3xl group hover:shadow-2xl cursor-pointer"
-							onClick={() => navigateToDetail(_.id)}
 						>
-							<div className={cn("w-full relative h-40 sm:h-[200px] rounded-2xl bg-cover  overflow-hidden")}>
-								<Image
-									className="group-hover:scale-110 object-cover"
-									src={_.image}
-									alt="banner"
-									fill
-								/>
-
+							<div className="w-full relative h-40 sm:h-[200px] rounded-2xl bg-cover overflow-hidden">
+								<Image className="group-hover:scale-110 object-cover" src={news.urlImage} alt="banner" fill />
 								<div className="absolute top-0 py-1 px-3 bg-grad text-white rounded-b-lg left-1/2 -translate-x-1/2">
-									{_.time}
+									{formatDate(news.createdDate)}
 								</div>
 							</div>
 							<div className="text-base font-semibold group-hover:text-gradient min-h-16 lg:min-h-20">
-								{truncateText(_.title, 85)}
+								{truncateText(news.mainTitle, 85)}
 							</div>
-							<div className="w-full flex items-center gap-3 transition-opacity duration-500 ease-in-out font-medium">
-								<div className="w-full border-t border-red-500 transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 ease-in-out"></div>
-								<p className="w-[280px] text-center px-1 text-black group-hover:text-red-500 text-sm">
-									Xem chi tiết
-								</p>
-								<div className="w-full border-t border-red-500 transform scale-x-0 group-hover:scale-x-100 origin-right transition-transform duration-500 ease-in-out"></div>
-							</div>
-						</div>
+						</Link>
 					))}
 				</div>
 			</div>
