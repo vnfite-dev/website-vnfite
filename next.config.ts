fc3f93c488fd80e32fddab60b5fc7cb90f2d38e9
Quang <ndquang191@gmail.com>
2025-02-28 11:36:46 +0700

    fix config and image new

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,33 @@
 import type { NextConfig } from "next";
-// import { WebUtils } from "./lib/utils";
+
+const formatTime = () => {
+	const now = new Date();
+	return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+};
+
+// Kiểm tra và thiết lập logger chỉ một lần
+if (!Reflect.has(globalThis, "__customLoggerSet")) {
+	Reflect.set(globalThis, "__customLoggerSet", true);
+
+	const originalConsoleLog = console.log;
+	console.log = (...args) => {
+		originalConsoleLog(`[${formatTime()}]`, ...args);
+	};
+
+	const originalConsoleError = console.error;
+	console.error = (...args) => {
+		originalConsoleError(`[${formatTime()}]`, ...args);
+	};
+
+	const originalConsoleWarn = console.warn;
+	console.warn = (...args) => {
+		originalConsoleWarn(`[${formatTime()}]`, ...args);
+	};
+}
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	images: {
-		remotePatterns: [{ hostname: "42.113.122.118" }], // Thêm hostname của hình ảnh
+		remotePatterns: [{ hostname: "42.113.122.118" }],
 	},
 };
 
