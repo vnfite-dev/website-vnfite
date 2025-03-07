65709a54bd704e7bee370e51bf1fd18a7a9212e6
Quang <ndquang191@gmail.com>
2025-03-07 17:27:07 +0700

    mini fix

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,12 +10,11 @@ import {
 import { cn, simpleFetchFunction } from "@/lib/utils";
 import { Calendar } from "lucide-react";
 import Image from "next/image";
-import { detailNews, detailPromotion } from "./data";
+import { detailNews } from "./data";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { Key, useEffect, useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-
 
 // const bigNews = [
 // 	{
@@ -72,7 +71,7 @@ const NewsPage = () => {
 				const newsList = [...(newsData ?? []), ...detailNews];
 				setNewsList(newsList);
 				const promotionData = await fetchNewsData(3);
-				setPromotionList([...(promotionData ?? []), ...detailPromotion]);
+				setPromotionList(promotionData);
 			} catch (error) {
 				console.error("Lỗi khi lấy dữ liệu:", error);
 			}
@@ -85,15 +84,14 @@ const NewsPage = () => {
 		setList([...newsList, ...promotionList]);
 	}, [newsList, promotionList]);
 
-	const filterSortList = list.sort((a: NewsItem, b: NewsItem) =>
-		new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime()
-	)
-	console.log('adada', filterSortList.length);
+	const filterSortList = list.sort(
+		(a: NewsItem, b: NewsItem) => new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime()
+	);
 
 	const filterPromotionList = promotionList.filter((item: NewsItem) => {
 		const createdDateItem = new Date(item.createdDate);
 		return createdDateItem >= timeAllowed;
-	})
+	});
 
 	const htmlToText = (html: string): string => {
 		if (!html) return "";
@@ -161,57 +159,53 @@ const NewsPage = () => {
 			<div className="mt-10 lg:mt-20">
 				<div className="text-center text-2xl lg:text-5xl font-semibold">Tin tức nổi bật</div>
 				<div className="mt-8 lg:mt-16 flex gap-8 xl:flex-row flex-col xl:items-start">
-					{
-						newsList[0] && (
+					{newsList[0] && (
+						<Link
+							href={`/news/${newsList[0]?.id}`}
+							className={cn(
+								"hidden sm:flex w-full md:w-[80%] lg:w-[55%] xl:w-full cursor-pointer rounded-4xl relative overflow-hidden group mx-auto aspect-square"
+							)}
+						>
+							<Image
+								src={newsList[0]?.urlImage ?? "/images/news/bigNew.jpg"}
+								alt="banner"
+								fill
+								className=""
+							/>
 
-							<Link
-								href={`/news/${newsList[0]?.id}`}
-								className={cn(
-									"hidden sm:flex w-full md:w-[80%] lg:w-[55%] xl:w-full cursor-pointer rounded-4xl relative overflow-hidden group mx-auto aspect-square"
-								)}
-							>
-								<Image
-									src={newsList[0]?.urlImage ?? "/images/news/bigNew.jpg"}
-									alt="banner"
-									fill
-									className=""
-								/>
+							{/* Gradient Filter for Bottom Half */}
+							<div className="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-gray-800/100 to-transparent group-hover:h-full group-hover:from-red-600/65 transition-all duration-300 pointer-events-none"></div>
 
-								{/* Gradient Filter for Bottom Half */}
-								<div className="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-gray-800/100 to-transparent group-hover:h-full group-hover:from-red-600/65 transition-all duration-300 pointer-events-none"></div>
+							{/* Content */}
+							<div className="relative z-10 flex justify-end items-center h-full px-7 pb-6 sm:pb-14 flex-col gap-4 overflow-hidden">
+								{/* Title Animation */}
+								<p className="text-white font-semibold text-lg sm:text-2xl transform translate-y-24 group-hover:-translate-y-0 transition-transform duration-500 ease-in-out">
+									{newsList[0]?.mainTitle}
+								</p>
 
-								{/* Content */}
-								<div className="relative z-10 flex justify-end items-center h-full px-7 pb-6 sm:pb-14 flex-col gap-4 overflow-hidden">
-									{/* Title Animation */}
-									<p className="text-white font-semibold text-lg sm:text-2xl transform translate-y-24 group-hover:-translate-y-0 transition-transform duration-500 ease-in-out">
-										{newsList[0]?.mainTitle}
-									</p>
-
-									{/* Detail Animation */}
-									{textContent && (
-										<>
-											<p className="hidden sm:flex sm:flex-col text-white text-sm opacity-0 group-hover:opacity-100 transform translate-y-16 group-hover:translate-y-0 transition-all duration-500 ease-in-out whitespace-pre-line">
-												{truncateText(textContent, 300)}
-											</p>
-											<p className="flex flex-col sm:hidden text-white text-sm opacity-0 group-hover:opacity-100 transform translate-y-16 group-hover:translate-y-0 transition-all duration-500 ease-in-out whitespace-pre-line">
-												{truncateText(textContent, 150)}
-											</p>
-										</>
-									)}
-
-									{/* Border Animation */}
-									<div className="w-full flex items-center gap-3 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
-										<div className="w-full border-t border-white transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 ease-in-out"></div>
-										<p className="w-[280px] text-center px-1 text-white text-sm sm:text-base">
-											Xem chi tiết
+								{/* Detail Animation */}
+								{textContent && (
+									<>
+										<p className="hidden sm:flex sm:flex-col text-white text-sm opacity-0 group-hover:opacity-100 transform translate-y-16 group-hover:translate-y-0 transition-all duration-500 ease-in-out whitespace-pre-line">
+											{truncateText(textContent, 300)}
 										</p>
-										<div className="w-full border-t border-white transform scale-x-0 group-hover:scale-x-100 origin-right transition-transform duration-500 ease-in-out"></div>
-									</div>
-								</div>
-							</Link>
-						)
-					}
+										<p className="flex flex-col sm:hidden text-white text-sm opacity-0 group-hover:opacity-100 transform translate-y-16 group-hover:translate-y-0 transition-all duration-500 ease-in-out whitespace-pre-line">
+											{truncateText(textContent, 150)}
+										</p>
+									</>
+								)}
 
+								{/* Border Animation */}
+								<div className="w-full flex items-center gap-3 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
+									<div className="w-full border-t border-white transform scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-500 ease-in-out"></div>
+									<p className="w-[280px] text-center px-1 text-white text-sm sm:text-base">
+										Xem chi tiết
+									</p>
+									<div className="w-full border-t border-white transform scale-x-0 group-hover:scale-x-100 origin-right transition-transform duration-500 ease-in-out"></div>
+								</div>
+							</div>
+						</Link>
+					)}
 
 					<div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 sm:px-[18%] md:px-0 lg:px-[10%] xl:px-0 mx-2">
 						{newsList?.slice(1, 5).map(
@@ -227,8 +221,8 @@ const NewsPage = () => {
 								<Link
 									key={index}
 									className="cursor-pointer group"
-									href={{ pathname: `/news/${_?.id}`}}
-								// onClick={() => navigateToDetail(_.id)}
+									href={{ pathname: `/news/${_?.id}` }}
+									// onClick={() => navigateToDetail(_.id)}
 								>
 									<div
 										className={cn(
@@ -244,8 +238,8 @@ const NewsPage = () => {
 											height={300}
 										/>
 									</div>
-									<div className="text-base font-semibold mt-2">
-										{truncateText(_.mainTitle, 85)}
+									<div className="text-base font-semibold mt-2 two-line-truncate">
+										{_.mainTitle}
 									</div>
 									<div className="flex gap-1 text-sm items-center text-gray-600">
 										<Calendar size={16} />
@@ -287,8 +281,8 @@ const NewsPage = () => {
 										{formatDate(news.createdDate)}
 									</div>
 								</div>
-								<div className="text-base font-semibold group-hover:text-gradient min-h-16 lg:min-h-20">
-									{truncateText(news.mainTitle, 85)}
+								<div className="text-base font-semibold group-hover:text-gradient  two-line-truncate">
+									{news.mainTitle}
 								</div>
 							</Link>
 						)
